d8aa5de2ed5b476e8c1216114229c526321221be
Corgi <hwanggumedia@gmail.com>
2021-03-01 08:34:42 +0900

    feat: Add progress #16 'portfolio dashboard'

--- front-end/src/components/Navigation/index.js
+++ front-end/src/components/Navigation/index.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const colorNavyBlue = '#1A1E43';
+const colorWhite = '#FFFFFF';
+
 const PrimaryNavMenuStyle = {
     Nav: styled.nav`
-        background-color: ;
+        max-width: 21em;
+        border-radius: 1.25em;
+        background-color: ${colorNavyBlue};
+        color: ${colorWhite};
+
+        & ul {
+            margin: 0;
+            padding: 0;
+            list-style-type: none;
+        }
     `
 };
 
 function PrimaryNavMenu() {
     return (
-        <Header>
-            <nav>
-                <div onClick={this.toggleMenu}><FontAwesomeIcon icon={faBars}/></div>
-                <Nav><strong><NavLink to="/">Hyungmo Gu</NavLink></strong></Nav>
-                <div></div>
-            </nav>
-        </Header>
+        <PrimaryNavMenuStyle.Nav>
+            <h2>Menu</h2>
+            <ul>
+                <li>Home</li>
+                <li>Info</li>
+                <li>Projects</li>
+                <li>Work Experience</li>
+            </ul>
+        </PrimaryNavMenuStyle.Nav>
     );
 };
 
