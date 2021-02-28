348787e0534879724a1cac80e051b69ba16fa91a
Corgi <hwanggumedia@gmail.com>
2021-03-01 08:21:47 +0900

    feat: Add progress #15 'portfolio dashboard'

--- front-end/src/screens/HomeScreen/index.js
+++ front-end/src/screens/HomeScreen/index.js
@@ -4,15 +4,8 @@ import styled from 'styled-components';
 class HomeScreen extends Component {
     render() {
         return (
-            <Screen>
-                <TypeWriter/>
-                <H1Extended>
-                    <div>Welcome to the</div>
-                    <div>portfolio site of</div>
-                    <div>Hyungmo Gu</div>
-                </H1Extended>
-                <strong>A Calgary based software developer who loves to learn, create new app, and push codes to github with his love</strong>
-            </Screen>
+           <>
+           </>
         );
     }
 }
