57c678ad4dfbec1193c084850a3ff639efefd04d
Corgi <hwanggumedia@gmail.com>
2021-03-02 13:34:18 +0900

    feat: Add progress #44 'portfolio dashboard'

--- front-end/src/components/Button/index.js
+++ front-end/src/components/Button/index.js
@@ -8,11 +8,20 @@ const ButtonStyle = {
         border-radius: ${constants.borderRadius};
         background-color: ${constants.colorNavyBlue};
         color: ${constants.colorWhite};
+        padding: 0.56rem;
+        cursor: pointer;
+    `,
+    Div: styled.div`
+        min-width: 6.09rem;
     `
 }
 
 export default function Button(props) {
     return(
-        <ButtonStyle.Button>{props.children}</ButtonStyle.Button>
+        <ButtonStyle.Button type={props.type}>
+            <ButtonStyle.Div>
+                {props.children}
+            </ButtonStyle.Div>
+        </ButtonStyle.Button>
     );
 }
