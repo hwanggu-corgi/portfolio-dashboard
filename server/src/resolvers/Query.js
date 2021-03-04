2b750941d053d2bc6d940d51a2f125ea86e1e530
Corgi <hwanggumedia@gmail.com>
2021-03-05 04:46:57 +0900

    feat: Add progress #88 'portfolio dashboard'

---
+++ server/src/resolvers/Query.js
@@ -0,0 +1,16 @@
+async function projects(parent, args, context) {
+    // get projects (with pagination) of user id
+    return
+}
+
+async function workExperiences(parent, args, context) {
+    // get work experiences (with pagination) of user id
+    return
+}
+
+
+async function user(parent, args, context) {
+    const { userId } = context;
+    // get user info of user id
+    return
+  }
