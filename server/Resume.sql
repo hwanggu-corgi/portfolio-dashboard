5c67c423e5821f43e34a459daea32562ec33cce7
Corgi <hwanggumedia@gmail.com>
2021-03-05 08:18:50 +0900

    feat: Add progress #94 'portfolio dashboard'

--- server/Resume.sql
+++ server/Resume.sql
@@ -4,14 +4,30 @@ CREATE TABLE Project (
     title       VARCHAR(100),
     date        DATE,
     shortDesc   VARCHAR(255),
-    techUsed    VARCHAR(100)[]
+    demoURL     VARCHAR(255),
+    sourceURL   VARCHAR(255),
+    techUsed    VARCHAR(100)[],
+    highLights  VARCHAR(255)[],
+    userId      INT,
+    FOREIGN KEY (userId) REFERENCES User(id)
 
 );
 
 DROP TABLE IF EXISTS WorkExperience;
 CREATE TABLE WorkExperience (
     id          SERIAL PRIMARY KEY,
-    title       VARCHAR(100),
-    date        DATE,
-    shortDesc   VARCHAR(255)
+    company     VARCHAR(100),
+    dateStart   DATE,
+    dateEnd     DATE,
+    location    VARCHAR(255),
+    highLights  VARCHAR(255)[],
+    userId      INT,
+    FOREIGN KEY (userId) REFERENCES User(id)
+);
+
+
+DROP TABLE IF EXISTS User;
+CREATE TABLE User (
+    id          SERIAL PRIMARY KEY,
+
 );
