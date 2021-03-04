20660a418164368d490eacff740f74a1c4290f83
Corgi <hwanggumedia@gmail.com>
2021-03-05 04:07:23 +0900

    feat: Add progress #87 'portfolio dashboard'

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,34 @@
 const { ApolloServer } = require('apollo-server');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const fs = require('fs');
+const path = require('path');
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
 
-// 2
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-}
+  Query,
+  Mutation,
+//   Chat,
+//   Subscription,
+};
 
-// 3
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
   resolvers,
-})
+  context: ({ req }) => {
+    return {
+      ...req,
+    };
+  }
+});
 
 server
   .listen()
   .then(({ url }) =>
     console.log(`Server is running on ${url}`)
 );
+
+// .listen(process.env.PORT)
