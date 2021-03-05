07cf1784f450166a5bb49e6f617390e465b4059a
Corgi <hwanggumedia@gmail.com>
2021-03-06 01:45:21 +0900

    feat: Add progress #106 'portfolio dashboard'

--- server/app.js
+++ server/app.js
@@ -6,25 +6,15 @@ const app = express();
 
 
 app.get("/projects", (req, res) => {
-    const text = "SELECT * FROM Projects";
+    const text = "SELECT * FROM projects";
     pool.query(text, (dbErr, dbRes) => {
         if (dbErr) res.status(500).send();
         res.send(dbRes);
     });
 });
 
-
-app.get("/work-experiences", (req, res) => {
-    const text = "SELECT * FROM WorkExperiences";
-    pool.query(text, (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
-        res.send(dbRes);
-    });
-});
-
-
-app.put("/work-experiences", (req, res) => {
-    const query = "INSERT INTO Projects()"
+app.post("/projects", (req, res) => {
+    const query = "INSERT INTO projects()"
 
     pool.query("INSERT", (dbErr, dbRes) => {
         if (dbErr) res.status(500).send();
@@ -34,6 +24,60 @@ app.put("/work-experiences", (req, res) => {
 
 
 app.put("/projects", (req, res) => {
+    const query = "INSERT INTO projects()"
+
+    pool.query("INSERT", (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send();
+        res.send(dbRes);
+    });
+});
+
+app.delete("/projects", (req, res) => {
+    const query = "INSERT INTO projects()"
+
+    pool.query("INSERT", (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send();
+        res.send(dbRes);
+    });
+});
+
+app.get("/work-experiences", (req, res) => {
+    const text = "SELECT * FROM work_experiences";
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send();
+        res.send(dbRes);
+    });
+});
+
+app.post("/work-experiences", (req, res) => {
+    const query = "INSERT INTO work_experiences()"
+
+    pool.query("INSERT", (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send();
+        res.send(dbRes);
+    });
+});
+
+
+app.put("/work-experiences", (req, res) => {
+    const query = "INSERT INTO work_experiences()"
+
+    pool.query("INSERT", (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send();
+        res.send(dbRes);
+    });
+});
+
+app.delete("/work-experiences", (req, res) => {
+    const query = "INSERT INTO work_experiences()"
+
+    pool.query("INSERT", (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send();
+        res.send(dbRes);
+    });
+});
+
+app.get("/info", (req, res) => {
     const query = "INSERT INTO Projects()"
 
     pool.query("INSERT", (dbErr, dbRes) => {
@@ -42,7 +86,7 @@ app.put("/projects", (req, res) => {
     });
 });
 
-app.put("/info", (req, res) => {
+app.post("/info", (req, res) => {
     const query = "INSERT INTO Projects()"
 
     pool.query("INSERT", (dbErr, dbRes) => {
@@ -50,5 +94,3 @@ app.put("/info", (req, res) => {
         res.send(dbRes);
     });
 });
-
-
