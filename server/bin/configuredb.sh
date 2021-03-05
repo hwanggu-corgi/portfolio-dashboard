29d412a733f6c784c406aec8ed371b0c8d218479
Corgi <hwanggumedia@gmail.com>
2021-03-05 11:01:48 +0900

    feat: Add progress #98 'portfolio dashboard'

--- server/bin/configuredb.sh
+++ server/bin/configuredb.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-dropdb -U node_user resumedb
-createdb -U node_user resumedb
+database="resumedb";
+
+echo "Configuring database: $resumedb";
+
+dropdb -U node_user resumedb;
+createdb -U node_user resumedb;
+
+psql -U node_user resumedb < ./bin/sql/resume.sql;
+
+echo "$resumedb configured";
