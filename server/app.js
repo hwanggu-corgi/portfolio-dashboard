df07897e291f68e8ebfd06e50c6a7ad6941d741e
Corgi <hwanggumedia@gmail.com>
2021-03-06 02:06:45 +0900

    feat: Add progress #107 'portfolio dashboard'

--- server/app.js
+++ server/app.js
@@ -8,89 +8,90 @@ const app = express();
 app.get("/projects", (req, res) => {
     const text = "SELECT * FROM projects";
     pool.query(text, (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
 
 app.post("/projects", (req, res) => {
-    const query = "INSERT INTO projects()"
-
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+    const text = "INSERT INTO projects() VALUES() RETURNING *";
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
-
 
 app.put("/projects", (req, res) => {
-    const query = "INSERT INTO projects()"
+    const text = "UPDATE projects SET () = () WHERE id = ___ RETURNING *";
 
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
 
-app.delete("/projects", (req, res) => {
-    const query = "INSERT INTO projects()"
+app.delete("/projects/:id", (req, res) => {
+    const text = "DELETE FROM projects WHERE id === $1";
+    const val = [req.params.id];
 
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
-        res.send(dbRes);
+    pool.query(text, val, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
+        res.status(204).send();
     });
 });
+
 
 app.get("/work-experiences", (req, res) => {
     const text = "SELECT * FROM work_experiences";
     pool.query(text, (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
 
-app.post("/work-experiences", (req, res) => {
-    const query = "INSERT INTO work_experiences()"
+app.post("/work-experiences/:id", (req, res) => {
+    const text = "INSERT INTO work_experiences() VALUES() RETURNING *";
 
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
 
 
-app.put("/work-experiences", (req, res) => {
-    const query = "INSERT INTO work_experiences()"
+app.put("/work-experiences/:id", (req, res) => {
+    const text = "UPDATE work_experiences SET () = () WHERE id = ___ RETURNING *";
 
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
 
-app.delete("/work-experiences", (req, res) => {
-    const query = "INSERT INTO work_experiences()"
+app.delete("/work-experiences/:id", (req, res) => {
+    const text = "DELETE FROM work_experiences WHERE id === $1";
 
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
+
 
 app.get("/info", (req, res) => {
-    const query = "INSERT INTO Projects()"
+    const text = "SELECT * FROM user"
 
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
 
 app.post("/info", (req, res) => {
-    const query = "INSERT INTO Projects()"
+    const text = "INSERT INTO user()"
 
-    pool.query("INSERT", (dbErr, dbRes) => {
-        if (dbErr) res.status(500).send();
+    pool.query(text, (dbErr, dbRes) => {
+        if (dbErr) res.status(500).send(dbErr);
         res.send(dbRes);
     });
 });
