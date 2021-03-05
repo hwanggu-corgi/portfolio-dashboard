d0576eef9220dd2fc1bed0ecc6515acd5411e42a
Corgi <hwanggumedia@gmail.com>
2021-03-05 13:47:15 +0900

    feat: Add progress #101 'portfolio dashboard'

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
+const { user, host, database, password, port } = require('../secrets/db_configuration');
 
 const Pool = new Pool({
-    user: 'resume_db_user',
-    host: 'localhost',
-    database: 'resumedb',
-    password: 'localhost',
-    port: 5434
+    user: user,
+    host: host,
+    database: database,
+    password: password,
+    port: port
 });
 
+pool.query('SELECT * FROM projects', (err, res) => {
+    if (err) return console.log(err);
+    console.log(res);
+});
