2a8c669cddd41a4cf7d7a72f1bbc7d297aa78337
Corgi <hwanggumedia@gmail.com>
2021-03-06 10:14:51 +0900

    feat: Add progress #120 'portfolio dashboard'

--- server/bin/sql/Resume.sql
+++ server/bin/sql/Resume.sql
@@ -1,48 +1,3 @@
-DROP TABLE IF EXISTS projects;
-CREATE TABLE project (
-    id          SERIAL PRIMARY KEY,
-    title       VARCHAR(100),
-    date        DATE,
-    shortDesc   VARCHAR(255),
-    demoURL     VARCHAR(255),
-    sourceURL   VARCHAR(255),
-    userId      INT,
-    FOREIGN KEY (userId) REFERENCES User(id)
-
-);
-
-DROP TABLE IF EXISTS work_experiences;
-CREATE TABLE work_experience (
-    id          SERIAL PRIMARY KEY,
-    company     VARCHAR(100),
-    dateStart   DATE,
-    dateEnd     DATE,
-    location    VARCHAR(255),
-    userId      INT,
-    FOREIGN KEY (userId) REFERENCES User(id)
-);
-
-DROP TABLE IF EXISTS highlights;
-CREATE TABLE highlights (
-    id          SERIAL PRIMARY KEY,
-    detail      VARCHAR(100),
-    workExpId   INT,
-    FOREIGN KEY (workExpId) REFERENCES work_experiences(id),
-    projectId   INT,
-    FOREIGN KEY (projectId) REFERENCES projects(id)
-);
-
-DROP TABLE IF EXISTS techUsed;
-CREATE TABLE techUsed (
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(100),
-    workExpId   INT,
-    FOREIGN KEY (workExpId) REFERENCES work_experiences(id),
-    projectId   INT,
-    FOREIGN KEY (projectId) REFERENCES projects(id)
-);
-
-
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
@@ -69,4 +24,49 @@ CREATE TABLE socials (
     value       VARCHAR(100),
     userId      INT,
     FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+
+DROP TABLE IF EXISTS projects;
+CREATE TABLE project (
+    id          SERIAL PRIMARY KEY,
+    title       VARCHAR(100),
+    date        DATE,
+    shortDesc   VARCHAR(255),
+    demoURL     VARCHAR(255),
+    sourceURL   VARCHAR(255),
+    userId      INT,
+    FOREIGN KEY (userId) REFERENCES user(id)
+
+);
+
+DROP TABLE IF EXISTS work_experiences;
+CREATE TABLE work_experience (
+    id          SERIAL PRIMARY KEY,
+    company     VARCHAR(100),
+    dateStart   DATE,
+    dateEnd     DATE,
+    location    VARCHAR(255),
+    userId      INT,
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+DROP TABLE IF EXISTS highlights;
+CREATE TABLE highlights (
+    id          SERIAL PRIMARY KEY,
+    detail      VARCHAR(100),
+    workExpId   INT,
+    FOREIGN KEY (workExpId) REFERENCES work_experiences(id),
+    projectId   INT,
+    FOREIGN KEY (projectId) REFERENCES projects(id)
+);
+
+DROP TABLE IF EXISTS techUsed;
+CREATE TABLE techUsed (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(100),
+    workExpId   INT,
+    FOREIGN KEY (workExpId) REFERENCES work_experiences(id),
+    projectId   INT,
+    FOREIGN KEY (projectId) REFERENCES projects(id)
 );
